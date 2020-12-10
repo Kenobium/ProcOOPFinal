a04cac93b1f3665d89b7454f34cc605481940c70
Dhruv <21991409+TheDoctor025@users.noreply.github.com>
2020-12-10 00:24:01 -0500

    Add saving

--- src/tk/thesenate/JPhotoEdit/Main.java
+++ src/tk/thesenate/JPhotoEdit/Main.java
@@ -1,7 +1,11 @@
 package tk.thesenate.JPhotoEdit;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -16,6 +20,7 @@ import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
 
+import javax.imageio.ImageIO;
 import java.io.*;
 import java.net.Authenticator;
 import java.net.URI;
@@ -24,6 +29,7 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.*;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.*;
+import java.nio.file.Files;
 import java.util.Optional;
 
 public class Main extends Application {
@@ -43,17 +49,33 @@ public class Main extends Application {
     public Button blurBackgroundButton;
     public Button contourButton;
     public Button sharpenButton;
-    public Slider slider0;
     public Label factorLabel;
-    public Label factorNumLabel;
     public Button uploadButton;
+    public TextField factorText = new TextField();
+    public Button saveButton;
     private String imgPath;
     private String imgFormat;
     private boolean imgSelected;
+    private boolean canSave;
+    private Image toSave;
     private double factor;
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @FXML
+    public void initialize() {
+        factorText.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*(\\.\\d*)?")) {
+                factorText.setText(oldValue);
+            }
+        });
+
+        factorText.setOnAction((actionEvent -> {
+            factor = Double.parseDouble(factorText.getText());
+            contrast(new ActionEvent());
+        }));
     }
 
     @Override
@@ -67,10 +89,7 @@ public class Main extends Application {
                 }
             }
         }));
-        /*slider0.valueProperty().addListener((observableValue, number, t1) -> {
-            factor = slider0.getValue();
-            factorNumLabel.setText(String.valueOf(factor));
-        });*/
+
         dir.deleteOnExit();
         Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
         Scene scene = new Scene(root);
@@ -81,7 +100,7 @@ public class Main extends Application {
 
     }
 
-    public void loadImage(ActionEvent actionEvent) {
+    public void load(ActionEvent actionEvent) {
         Node node = (Node) actionEvent.getSource();
         Stage stage = (Stage) node.getScene().getWindow();
         FileChooser fileChooser = new FileChooser();
@@ -107,6 +126,22 @@ public class Main extends Application {
             imgSelected = true;
         }
 
+    }
+
+    public void save(ActionEvent actionEvent) {
+        Node node = (Node) actionEvent.getSource();
+        Stage stage = (Stage) node.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save");
+        fileChooser.getExtensionFilters().addAll(
+                new ExtensionFilter("Image Files", "*.png", "*.jpg"),
+                new ExtensionFilter("All Files", "*.*")
+        );
+        File initial = new File(imgPath);
+        fileChooser.setInitialDirectory(new File(initial.getParent()));
+        fileChooser.setInitialFileName(initial.getName());
+        File f = fileChooser.showSaveDialog(stage);
+        imgToFile(toSave, imgFormat, f);
     }
 
     public void blurBackground(ActionEvent actionEvent) {
@@ -158,14 +193,16 @@ public class Main extends Application {
     public void contrast(ActionEvent actionEvent) {
         if (imgSelected) {
             try {
-                slider0.setVisible(true);
-                double factor = slider0.getValue();
+                factorText.setDisable(false);
+                factor = 1.6;
+                factorText.setText(String.valueOf(factor));
                 Process p = Runtime.getRuntime().exec("python ./src/img/Contrast.py " + imgPath + " " + factor);
                 showOutput(p);
                 p.waitFor();
                 FileInputStream f = new FileInputStream("./cache/cr_temp." + imgFormat);
                 afterImg.setImage(new Image(f));
                 f.close();
+                toSave = afterImg.getImage();
             } catch (IOException | InterruptedException e) {
                 e.printStackTrace();
             }
@@ -294,12 +331,21 @@ public class Main extends Application {
                 .map(f -> f.substring(filename.lastIndexOf(".") + 1));
     }
 
+    private File imgToFile(Image image, String format, File file) {
+        try {
+            ImageIO.write(SwingFXUtils.fromFXImage(image, null), format, file);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return file;
+    }
+
     public void upload(ActionEvent actionEvent) {
         HttpClient client = HttpClient.newBuilder()
                 .version(HttpClient.Version.HTTP_2)
                 .build();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("http://ptsv2.com/t/society/post"))
+                .uri(URI.create("http://gallery.thesenate.tk/"))
                 .POST(BodyPublishers.ofString("hello there"))
                 .build();
         try {
@@ -310,4 +356,6 @@ public class Main extends Application {
         }
 
     }
+
+
 }
